6393e689956ea559bfd88de376c972cc3c645717
rayan benhamed <benhamedrayano@gmail.com>
2024-10-30 12:38:05 +0100

    first commit, react router dom is working!

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from './Components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contacts from './pages/Contacts';
+import {Routes, Route } from 'react-router-dom';
+
+function App() {
+
+    return (
+        <div>
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/products' element={<Products/>}/>
+                <Route path='/about' element={<About/>}/>
+                <Route path='/contacts' element={<Contacts/>}/>
+            </Routes>
+
+        </div>
+
+    )
+
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Contacts</li>
+            </ul>
+            <button>Get Started</button>
+        </div>
+    )
+} 
+export default Navbar
